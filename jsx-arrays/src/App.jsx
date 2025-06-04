ceab268543ca87fc16325b00e52fb08bdc007448
Wikten Alves <wiktenalves@gmail.com>
2025-06-04 19:33:45 -0300

    Aula  Arrays jsx correção

--- jsx-arrays/src/App.jsx
+++ jsx-arrays/src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const produtos = [
   {
     id: 1,
@@ -20,15 +22,26 @@ const produtos = [
 ];
 
 const App = () => {
-  const dados = produtos.filter(({preco}) => Number=(preco.replace("R$ ", "")) > 1500)
-  return <section>
-    {dados.map(({id, nome, preco, cores}) => <div key={id}>
-      <h1>{nome}</h1>
-      <p>Preço: {preco}</p>
-      <ul>{cores.map((cor) => (
-        <li style={{backgroundColor: cor, color: "white"}} key={cor}>{cor}</li>
-      ))}</ul>
-    </div>
-  )}
-  </section>;
+  const dados = produtos.filter(
+    ({ preco }) => Number(preco.replace('R$ ', '')) > 1500,
+  );
+  return (
+    <section>
+      {dados.map(({ id, nome, preco, cores }) => (
+        <div key={id}>
+          <h1>{nome}</h1>
+          <p>Preço: {preco}</p>
+          <ul>
+            {cores.map((cor) => (
+              <li style={{ backgroundColor: cor, color: 'white' }} key={cor}>
+                {cor}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </section>
+  );
 };
+
+export default App;
